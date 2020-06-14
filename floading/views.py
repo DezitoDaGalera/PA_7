39764f9b9ae2038e20f38bd842096ae965b2ff41
André Chierighini <andrechierighini2405@gmail.com>
2020-06-14 19:57:23 -0300

    presentation time

--- floading/views.py
+++ floading/views.py
@@ -1,5 +1,4 @@
 from .models import endereco
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, renderer_classes
