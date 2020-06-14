1159294d98e99514d4624ca9fcb10ed246c0c963
André Chierighini <andrechierighini2405@gmail.com>
2020-06-14 00:10:29 -0300

    update settings

--- api_pa/settings.py
+++ api_pa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')^-%ia*)qlo6&zw1he2f7gu1z)7jd4xt6*1y7tqlu60)zkny(('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,15 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
